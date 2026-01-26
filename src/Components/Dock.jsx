f0b6c4709f0e07f6ec3c1909dfb958e3af1191d6
Rohit Pandey <rp5120523@gmail.com>
2026-01-26 09:35:29 +0530

    created dock and applied transition effects

--- src/Components/Dock.jsx
+++ src/Components/Dock.jsx
@@ -1,31 +1,31 @@
 import React from "react";
-
+import "./dock.scss";
 const Dock = () => {
   return (
     <footer className="dock">
-      <div className="icon">
-        <img src="public\doc-icons\Assets\spotify.svg" alt="icon" />
+      <div className="icon spotify">
+        <img src="\doc-icons\Assets\spotify.svg" alt="icon" />
       </div>
-      <div className="icon">
-        <img src="public\doc-icons\Assets\calendar.svg" alt="icon" />
+      <div className="icon calendar">
+        <img src="\doc-icons\Assets\calendar.svg" alt="icon" />
       </div>
-      <div className="icon">
-        <img src="public\doc-icons\Assets\cli.svg" alt="icon" />
+      <div className="icon cli">
+        <img src="\doc-icons\Assets\cli.svg" alt="icon" />
       </div>
-      <div className="icon">
-        <img src="public\doc-icons\Assets\github.svg" alt="icon" />
+      <div className="icon github">
+        <img src="\doc-icons\Assets\github.svg" alt="icon" />
       </div>
-      <div className="icon">
-        <img src="public\doc-icons\Assets\lick.svg" alt="icon" />
+      <div className="icon lick">
+        <img src="\doc-icons\Assets\lick.svg" alt="icon" />
       </div>
-      <div className="icon">
-        <img src="/public\doc-icons\Assets\mail.svg" alt="icon" />
+      <div className="icon mail">
+        <img src="\doc-icons\Assets\mail.svg" alt="icon" />
       </div>
-      <div className="icon">
-        <img src="public\doc-icons\Assets\notes.svg" alt="icon" />
+      <div className="icon notes">
+        <img src="\doc-icons\Assets\notes.svg" alt="icon" />
       </div>
-      <div className="icon">
-        <img src="public\doc-icons\Assets\pdf.svg" alt="icon" />
+      <div className="icon pdf">
+        <img src="\doc-icons\Assets\pdf.svg" alt="icon" />
       </div>
     </footer>
   );
