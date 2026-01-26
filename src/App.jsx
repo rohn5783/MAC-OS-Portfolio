9c4f69dd17d22877273991b95a697befae95796b
Rohit Pandey <rp5120523@gmail.com>
2026-01-26 08:40:16 +0530

    creted basic layout

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.scss'
+import Dock from './Components/Dock'
+
+
+
+
+const App = () => {
+  return (
+  <main>
+<Dock />
+  </main>
+  )
+}
+
+export default App
---
+++ src/Components/Dock.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Dock = () => {
+  return (
+    <footer className="dock">
+      <div className="icon">
+        <img src="public\doc-icons\Assets\spotify.svg" alt="icon" />
+      </div>
+      <div className="icon">
+        <img src="public\doc-icons\Assets\calendar.svg" alt="icon" />
+      </div>
+      <div className="icon">
+        <img src="public\doc-icons\Assets\cli.svg" alt="icon" />
+      </div>
+      <div className="icon">
+        <img src="public\doc-icons\Assets\github.svg" alt="icon" />
+      </div>
+      <div className="icon">
+        <img src="public\doc-icons\Assets\lick.svg" alt="icon" />
+      </div>
+      <div className="icon">
+        <img src="/public\doc-icons\Assets\mail.svg" alt="icon" />
+      </div>
+      <div className="icon">
+        <img src="public\doc-icons\Assets\notes.svg" alt="icon" />
+      </div>
+      <div className="icon">
+        <img src="public\doc-icons\Assets\pdf.svg" alt="icon" />
+      </div>
+    </footer>
+  );
+};
+
+export default Dock;
